72adc2dbfecaedf7103cf1aab9cefeeea3129e98
kdsreis <kleber.embraer@gmail.com>
2023-01-28 09:57:43 -0300

    feat: Azure Key Vault

--- src/1 - AvanadeEstudosAPI.API/Program.cs
+++ src/1 - AvanadeEstudosAPI.API/Program.cs
@@ -6,6 +6,9 @@ using AvanadeEstudosAPI.Profiles;
 using AvanadeEstudosAPI.Services.Interfaces;
 using AvanadeEstudosAPI.Services.Services;
 using Microsoft.EntityFrameworkCore;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +18,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Configuration.AddAzureKeyVault(
+       "https://kvestudosapi.vault.azure.net/",
+       "7f3777b3-1ed4-42ad-a248-5f74da7e165f",
+       "a9t8Q~WeqZCEU2ZChArvETYYa9HpGfZqyIKv6dho");
+
 
 builder.Services.AddDbContext<AvanadeEstudosAPIContext>(options => options.UseSqlServer(
 builder.Configuration.GetConnectionString("DefaultConnection")
@@ -26,6 +36,7 @@ builder.Services.AddTransient<IUserRepository, UserRepository>();
 var automapper = new MapperConfiguration(item => item.AddProfile(new UserProfile()));
 IMapper mapper = automapper.CreateMapper();
 builder.Services.AddSingleton(mapper);
+
 
 var app = builder.Build();
 
